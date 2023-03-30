3a036b234d1ec10f880d0c47a10752b5451ee251
trevortamu22 <trevor.sides@tamu.edu>
2023-03-30 14:22:32 -0500

    Integration Updates

--- Motor_Controller_Arduino.ino
+++ Motor_Controller_Arduino.ino
@@ -1,25 +1,50 @@
 #include <AccelStepper.h>
 
 // Defining the Motors 
-AccelStepper motor1(AccelStepper::DRIVER, 3, 4); //(step pin, direction pin)
-AccelStepper motor2(AccelStepper::DRIVER, 6, 7);
-AccelStepper motor3(AccelStepper::DRIVER, 9, 10);
+AccelStepper motor1(AccelStepper::DRIVER, 3, 4, 5); //(step pin, direction pin, enable pin)
+AccelStepper motor2(AccelStepper::DRIVER, 6, 7, 8);
+AccelStepper motor3(AccelStepper::DRIVER, 9, 10, 11);
+
+// Setting Motor Homing
+const int home_delay = 2000; //ms
+// Setting the Stepper Motors Speed and Acceleration
+///////////// MOTOR ONE ///////////////
+const int mtr1_maxspeed = 2000;
+const int mtr1_acceleration = 25000;
+//const int mtr1_speed = 500000;
+//Return Conditions
+const int mtr1_rmaxspeed = 1000;
+const int mtr1_racceleration = 500;
+////////////// MOTOR TWO //////////////
+const int mtr2_maxspeed = 2000;
+const int mtr2_acceleration = 25000;
+//const int mtr2_speed = 500000;
+//Return Conditions
+const int mtr2_rmaxspeed = 1000;
+const int mtr2_racceleration = 500;
+//////////// MOTOR THREE ///////////////
+const int mtr3_maxspeed = 2000;
+const int mtr3_acceleration = 25000;
+//const int mtr3_speed = 500000;
+//Return Conditions
+const int mtr3_rmaxspeed = 1000;
+const int mtr3_racceleration = 500;
 
 void setup() {
   //setting the serial communication 
   Serial.begin(9600);
   // Setting the maximum speed and acceleration of each motor
-  motor1.setMaxSpeed(1000);
-  motor1.setAcceleration(500);
-  motor2.setMaxSpeed(2000);
-  motor2.setAcceleration(1000);
-  motor3.setMaxSpeed(2000);
-  motor3.setAcceleration(1000);
+  motor1.setMaxSpeed(mtr1_maxspeed);
+  motor1.setAcceleration(mtr1_acceleration);
+  motor2.setMaxSpeed(mtr2_maxspeed);
+  motor2.setAcceleration(mtr2_acceleration);
+  motor3.setMaxSpeed(mtr3_maxspeed);
+  motor3.setAcceleration(mtr3_acceleration);
 }
 
 void MoveMotors(int move1, int move2, int move3) {
   // Move the desired amount of steps 
-  motor1.moveTo(move1);
+  motor1.moveTo(-move1);
   motor2.moveTo(move2);
   motor3.moveTo(move3);
 
@@ -29,17 +54,17 @@ void MoveMotors(int move1, int move2, int move3) {
     motor2.run();
     motor3.run();
   }
-  delay(2000);
+  delay(home_delay);
 } 
 
 void ReturnHome() {
   //Changing speed and acceleration slower
-  motor1.setMaxSpeed(1000);
-  motor1.setAcceleration(500);
-  motor2.setMaxSpeed(1000);
-  motor2.setAcceleration(500);
-  motor3.setMaxSpeed(1000);
-  motor3.setAcceleration(500);
+  motor1.setMaxSpeed(mtr1_rmaxspeed);
+  motor1.setAcceleration(mtr1_racceleration);
+  motor2.setMaxSpeed(mtr2_rmaxspeed);
+  motor2.setAcceleration(mtr2_racceleration);
+  motor3.setMaxSpeed(mtr3_rmaxspeed);
+  motor3.setAcceleration(mtr3_racceleration);
 
   // Move each motor in the opposite direction to return to home 
   motor1.moveTo(0);
@@ -52,11 +77,17 @@ void ReturnHome() {
     motor2.run();
     motor3.run();
   }
-  while (1) {
-  }
+  //Changing speed and acceleration slower
+  motor1.setMaxSpeed(mtr1_maxspeed);
+  motor1.setAcceleration(mtr1_acceleration);
+  motor2.setMaxSpeed(mtr2_maxspeed);
+  motor2.setAcceleration(mtr2_acceleration);
+  motor3.setMaxSpeed(mtr3_maxspeed);
+  motor3.setAcceleration(mtr3_acceleration);
+  delay(1000);
 }
 
-//Connecting to Python and calling functions
+//Connecting to Python and Calling Functions
 void loop() {
   if (Serial.available() > 0) {
     //Reading the incoming command from python 
--- motor_controller.py
+++ motor_controller.py
@@ -5,18 +5,29 @@ print("start")
 # Connect to Arduino
 
 def connect():
-    return serial.Serial('COM3', 9600, timeout=1)
+    ser = serial.Serial('COM3', 9600, timeout=1)
+    time.sleep(0.5)
+    return ser
 
 def move(ser, m1,m2,m3): 
-# Send command to move motors
+    # Protect Motor Limits
+    print('Moving to', m1, m2, m3)
+    if m1 >= 500: m1 = 500
+    if m2 >= 500: m2 = 500
+    if m3 >= 500: m3 = 500
+    
+    # Send command to move motors
     ser.write(b'Move\n')
     ser.write(str(m1).encode() + b'\n')
     ser.write(str(m2).encode() + b'\n')
     ser.write(str(m3).encode() + b'\n')
+    
 
 def returnhome(ser):
 # Send command to return to home
+    #move(ser, -global_m1, -global_m2, -global_m3)
     ser.write(b'ReturnHome\n')
+    time.sleep(1)
 
 
 
