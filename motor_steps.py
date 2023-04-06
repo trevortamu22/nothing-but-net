39c3454ec071a7521a9c2db4b52dfe0599ceaef0
trevortamu22 <trevor.sides@tamu.edu>
2023-04-06 12:30:48 -0500

    Update motor_steps.py

--- motor_steps.py
+++ motor_steps.py
@@ -3,13 +3,6 @@ import numpy as np
 # Function that calculates the necessary motor steps to make the desired backboard position happen.
 # The inputs to this function are Blaine's theta_z and theta_x, and the three solution matrices from the motion study.
 def motor_steps(theta_z, theta_x, motor1_array, motor2_array, motor3_array):
-    # make an array with difference between Blaine's theta_z and my theta_z matrix. The minumum value's...
-    # index in this array corresonds to the motor position we need in the solution matrix
-    theta_z_difference_array = motor3_array[1] - theta_z
-    theta_z_difference_array = np.absolute(theta_z_difference_array)
-    index = np.argmin(theta_z_difference_array)
-    motor3_step_command = motor3_array[0][index]
-    
     # make an array with difference between Blaine's theta_x and my theta_x matrix. Then find the index of the minum difference
     # we will use the motor1_array here
     theta_x_difference_array = motor1_array[2] - theta_x
@@ -18,8 +11,35 @@ def motor_steps(theta_z, theta_x, motor1_array, motor2_array, motor3_array):
     
     # now divide that index by 2 becuase we will rotate motor 2 in the opposite direction. This way...
     # the theta_z effects cancel out and the theta_x effects add to each other
-    index = index // 2  # this needs to be an integer
-    motor1_step_command = motor3_array[0][index]
-    motor2_step_command = -1*motor1_step_command
+    # index = index // 2  # this needs to be an integer
+    motor1_step_command = motor1_array[0][index]
+    theta_z_motor1 = motor1_array[1][index]     #theta_z from motor 1 will be used later
+    # motor2_step_command = -1*motor1_step_command
+    motor2_step_command = 0
+    
+
+    # now control motor 3 in order to achieve the rest of theta_z that needs to be reached
+    theta_z_from_1 = theta_z_motor1
+    theta_z_needed = theta_z - theta_z_from_1
+    
+    # make an array with difference between Blaine's theta_z and my theta_z matrix. The minumum value's...
+    # index in this array corresonds to the motor position we need in the solution matrix
+    theta_z_difference_array = motor3_array[1] - theta_z_needed
+    theta_z_difference_array = np.absolute(theta_z_difference_array)
+    index = np.argmin(theta_z_difference_array)
+    motor3_step_command = motor3_array[0][index]
+    
+    # # make an array with difference between Blaine's theta_z and my theta_z matrix. The minumum value's...
+    # # index in this array corresonds to the motor position we need in the solution matrix
+    # theta_z_difference_array = motor3_array[1] - theta_z
+    # theta_z_difference_array = np.absolute(theta_z_difference_array)
+    # index = np.argmin(theta_z_difference_array)
+    # motor3_step_command = motor3_array[0][index]
+    
+    
+    motor3_step_command = motor3_step_command // 2
+    motor1_step_command += -1*(motor3_step_command)
+    motor2_step_command += -1*(motor3_step_command)
+    
     
     return motor1_step_command, motor2_step_command, motor3_step_command
